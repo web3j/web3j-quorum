7406c918ec7651eed7a0043a015837a6f53c3b50
sebaraba <rabasebastians@gmail.com>
2018-11-30 14:48:59 +0000

    Refactor code

--- src/main/kotlin/org/web3j/quorum/enclave/Tessera.kt
+++ src/main/kotlin/org/web3j/quorum/enclave/Tessera.kt
@@ -5,8 +5,9 @@ import org.web3j.quorum.Quorum
 import org.web3j.quorum.enclave.protocol.EnclaveService
 
 class Tessera(private val service: EnclaveService, private val web3: Quorum) : Enclave(service) {
-    // move web3 to the cosntructor
-
+    /**
+     * Send the encrypted payload to the Tessera's endpoint
+     */
     override fun sendRawRequest(payload: String, privateFor: List<String>): EthSendTransaction {
         return web3.ethSendRawPrivateTransaction(payload, privateFor).send()
     }
@@ -14,17 +15,16 @@ class Tessera(private val service: EnclaveService, private val web3: Quorum) : E
     /**
      * Store a new payload to Enclave for secure enclave.
      */
-    fun storeRawRequest(payload: String, from: String): SendResponse {
-        val storerawRequest = StoreRawRequest(payload, from)
-        return service.send(storerawRequest, "storeraw", SendResponse::class.java)
+    override fun storeRawRequest(payload: String, from: String, to: List<String>): SendResponse {
+        val storeRawRequest = StoreRawRequest(payload, from)
+        return service.send(storeRawRequest, "storeraw", SendResponse::class.java)
     }
 
     /**
      * Verify that our node is running
      */
-    override fun upCheck() : Boolean {
+    override fun upCheck(): Boolean {
         val test = service.send("upcheck")
-        return test  == "I'm up!"
+        return test == "I'm up!"
     }
-
 }
