9db387e2a986c0dd2595c657e9ae7ba3456e61a0
apratt3377 <apratt3377@gmail.com>
2020-08-27 07:19:48 -0400

    add missing raft and permission rpcs

--- src/main/java/org/web3j/quorum/methods/response/raft/RaftPromote.java
+++ src/main/java/org/web3j/quorum/methods/response/raft/RaftPromote.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Web3 Labs Ltd.
+ * Copyright 2020 Web3 Labs Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -10,6 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.quorum.methods.response.raft
+package org.web3j.quorum.methods.response.raft;
 
-public data class RaftPeer(var hostname: String, var nodeActive: String, var nodeId: String, var p2pPort: String, var raftId: String, var raftPort: String, var role: String)
+import org.web3j.protocol.core.Response;
+
+public class RaftPromote extends Response<Boolean> {
+
+    public Boolean getPromotionStatus() {
+        return getResult();
+    }
+}
