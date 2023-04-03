4ce6cf100bc449577447756171671a0d36d51fd3
Krishnan Narayanan <krishnan.narayanan@partior.com>
2023-04-03 08:36:45 +0000

    Proper naming of the methods and classes

--- src/integration-test/java/org/web3j/quorum/ExtensionResponseTest.java
+++ src/integration-test/java/org/web3j/quorum/ExtensionResponseTest.java
@@ -98,7 +98,7 @@ public class ExtensionResponseTest extends ResponseTester {
     }
 
     @Test
-    public void testgquorumExtensionGenerateExtensionApprovalUuid() {
+    public void testquorumExtensionGenerateExtensionApprovalUuid() {
         buildResponse(
                 "{\n"
                         + "  \"jsonrpc\":\"2.0\",\n"
